1acf55b7c798aee696da246e9d5d410f3394992e
bentwhiskers <abbott1414@gmail.com>
2023-07-24 17:05:36 -0400

    added MiniCssExtractPlugin and css-loader for handling CSS files; included babel, with its preset-env to allow the latest JavaScript features to be used

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,9 +2,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // For CSS 
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,12 +17,42 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/index.html'
+      }),
+      new WebpackPwaManifest({
+        name: 'My Progressive Web App',
+        short_name: 'MyPWA',
+        description: 'My awesome Progressive Web App!',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials', // can be null, use-credentials or anonymous
+      }),
+      new InjectManifest({
+        swSrc: './src/service-worker.js',
+      }),
+      new MiniCssExtractPlugin({ // For CSS
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
       ],
     },
   };
